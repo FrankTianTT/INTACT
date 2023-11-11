4c34f7f09fb329e758265e6f32b6270ad8a1f740
frank <franktian424@gmail.com>
2023-11-12 00:07:11 +0800

    :tada: take causal-mask and context-model as nn.Module

---
+++ tdfa/models/causal_mask.py
@@ -0,0 +1,78 @@
+import torch
+from torch import nn
+from torch.distributions import Bernoulli
+
+
+class CausalMask(nn.Module):
+    def __init__(
+            self,
+            observed_input_dim,
+            context_input_dim,
+            mask_output_dim,
+            logits_clip=3.0,
+    ):
+        super().__init__()
+
+        self.observed_input_dim = observed_input_dim
+        self.context_input_dim = context_input_dim
+        self.mask_output_dim = mask_output_dim
+        self.logits_clip = logits_clip
+
+        self._mask_logits = nn.Parameter(torch.randn(self.mask_output_dim, self.mask_input_dim))
+
+    @property
+    def mask_input_dim(self):
+        return self.observed_input_dim + self.context_input_dim
+
+    @property
+    def mask(self):
+        return torch.gt(self.mask_logits, 0).int()
+
+    @property
+    def mask_logits(self):
+        return torch.clamp(self._mask_logits, -self.logits_clip, self.logits_clip)
+
+    def forward(self, inputs, dim_map=None, deterministic=False):
+        assert len(inputs.shape) == 2, "inputs should be 2D tensor: batch_size x input_dim"
+        batch_size, input_dim = inputs.shape
+        if dim_map is None:
+            assert input_dim == self.mask_input_dim, \
+                "dimension of inputs should be equal to mask_input_dim if dim_map is None"
+
+        # shape: mask_output_dim * batch_size * input_dim
+        repeated_inputs = inputs.unsqueeze(0).expand(self.mask_output_dim, -1, -1)
+
+        if deterministic:
+            mask = torch.gt(self.mask_logits, 0).float().expand(batch_size, -1, -1)
+        else:
+            mask = Bernoulli(logits=self.mask_logits).sample(torch.Size([batch_size]))
+
+        if dim_map is not None:
+            mask = mask[:, :, dim_map]
+
+        masked_inputs = torch.einsum("boi,obi->obi", mask, repeated_inputs)
+
+        return masked_inputs, mask
+
+
+if __name__ == '__main__':
+    observed_input_dim = 5
+    context_input_dim = 10
+    real_input_dim = 100
+    mask_output_dim = 20
+    batch_size = 5
+
+    mask = CausalMask(
+        observed_input_dim=observed_input_dim,
+        context_input_dim=context_input_dim,
+        mask_output_dim=mask_output_dim,
+    )
+
+    inputs = torch.randn(batch_size, real_input_dim)
+    dim_map = torch.randint(0, observed_input_dim + context_input_dim, (real_input_dim,))
+    print(dim_map)
+
+    masked_inputs, mask = mask(inputs, dim_map=dim_map)
+
+    print(masked_inputs.shape)
+    print(mask.shape)
---
+++ tdfa/models/context_model.py
@@ -0,0 +1,46 @@
+import torch
+from torch import nn
+
+
+class ContextModel(nn.Module):
+    def __init__(
+            self,
+            max_context_dim=0,
+            task_num=0,
+    ):
+        super().__init__()
+        self.max_context_dim = max_context_dim
+        self.task_num = task_num
+
+        self.context_hat = torch.nn.Parameter(torch.randn(task_num, max_context_dim))
+
+    @property
+    def is_meta(self):
+        return self.max_context_dim > 0 and self.task_num > 0
+
+    def forward(self, idx):
+        batch_size = idx.shape[0]
+
+        if self.is_meta:
+            return self.context_hat[idx.squeeze()]
+        else:
+            return torch.empty(batch_size, 0).to(self.context_hat.device)
+
+
+if __name__ == '__main__':
+    max_context_dim = 10
+    task_num = 10
+    batch_size = 5
+
+    context_model = ContextModel(
+        max_context_dim=max_context_dim,
+        task_num=task_num,
+    )
+
+    idx = torch.randint(0, task_num, (batch_size, 1))
+
+    context = context_model(idx)
+
+    print(context.shape)
+
+    print(context_model.parameters())
