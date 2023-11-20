0f7e81a585ccf0b44196218b5f7df145a06c3ec4
frank <franktian424@gmail.com>
2023-11-20 21:49:36 +0800

    :bug: fix ContextModel dimension bug

--- tdfa/modules/models/context_model.py
+++ tdfa/modules/models/context_model.py
@@ -19,10 +19,11 @@ class ContextModel(nn.Module):
         return self.max_context_dim > 0 and self.task_num > 0
 
     def forward(self, idx):
+        assert len(idx.shape) == 2 and idx.shape[1] == 1
         batch_size = idx.shape[0]
 
         if self.is_meta:
-            return self.context_hat[idx.squeeze()]
+            return self.context_hat[idx.reshape(-1)]
         else:
             return torch.empty(batch_size, 0).to(self.context_hat.device)
 
